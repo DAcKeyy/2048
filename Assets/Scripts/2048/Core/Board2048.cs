cb1e0150accc7ff82aa027fc01f82f280549ae4b
DovmantovAK <alexanderdovmantov@gmail.com>
2022-07-12 23:37:54 +0300

    fix: Node2048 CheckPossibleCombinations() and GetNeighbour()

--- Assets/Scripts/2048/Core/Board2048.cs
+++ Assets/Scripts/2048/Core/Board2048.cs
@@ -9,11 +9,12 @@ namespace _2048.Core
 {
     public class Board2048
     {
-        public Dictionary<Vector2UInt, Node2048> NodeGrid { get; private set; }
+        public List<Node2048> NodeGrid { get; private set; }
         public Vector2UInt BoardBounds { get; private set; }
         
         public Action<Node2048> NodeAdded = delegate(Node2048 node2048) {  };
         public Action<Node2048> NodeRemoved = delegate(Node2048 node2048) {  };
+        public Action GameOver = delegate() {  };
 
         public Board2048(Vector2UInt boardBounds)
         {
@@ -21,24 +22,12 @@ namespace _2048.Core
                 throw new Exception("Board bounds too small");
             
             BoardBounds = boardBounds;
-            NodeGrid = new Dictionary<Vector2UInt, Node2048>();
-            InstantiateBoard();
-        }
-        
-        private void InstantiateBoard()
-        {
+            NodeGrid = new List<Node2048>();
+            
             NodeGrid.Clear();
             InitStartNodes((uint)Random.Range(1,3));
-            
-            for (var column = 0; column < BoardBounds.x; column++)
-            {
-                for (var row = 0; row < BoardBounds.y; row++)
-                {
-                    
-                }
-            }
         }
-    
+
         private bool InstantiateNode(uint x, uint y, out Node2048? node)
         {
             node = null;
@@ -46,9 +35,9 @@ namespace _2048.Core
             if (x >= BoardBounds.x) return false;
             if (y >= BoardBounds.y) return false;
                 
-            var existNode = NodeGrid.FirstOrDefault(gridElem => gridElem.Key == new Vector2UInt(x,y));
+            var existNode = NodeGrid.FirstOrDefault(gridElem => gridElem.Position == new Vector2UInt(x,y));
 
-            if (existNode.Key == new Vector2UInt(x, y))
+            if (existNode != null && existNode.Position == new Vector2UInt(x, y))
             {
                 var position = new Vector2UInt(x, y);
                 node = new Node2048(position, (uint)Random.Range(2, 4));
@@ -58,20 +47,22 @@ namespace _2048.Core
             else return false;
         }
 
-        private void MoveNode(ref Node2048 fromNode, Vector2UInt position)
+        private bool MoveNode(ref Node2048 fromNode, Vector2UInt newPosition)
         {
-            
+            var node = NodeGrid.First(nodePos => nodePos.Position == newPosition);
+            if (node != null) return false;
+
+            fromNode.Position = newPosition;
+            return true;
         }
         
         public bool Combine(Node2048 fromNode, Node2048 toNode, out Node2048? newNode)
         {
             if (fromNode.Value == toNode.Value)
             {
+                newNode = new Node2048(toNode.Position, fromNode.Value + toNode.Value);
                 RemoveNode(toNode);
                 RemoveNode(fromNode);
-                
-                newNode = new Node2048(toNode.Position, fromNode.Value + toNode.Value);
-                
                 AddNode(newNode);
             }
 
@@ -81,14 +72,65 @@ namespace _2048.Core
         
         public void Move(Direction direction)
         {
+            foreach (var node in NodeGrid)
+            {
+                var cortage = node.PossibleCombinations.First(collection => collection.direction == direction);
+                
+                if(cortage.possibleNode == null) continue;
+                
+                Combine(node, cortage.possibleNode, out var newNode);
+            }
+            
             if (direction == Direction.Top)
             {
-                
+                for (var x = BoardBounds.x - 1;
+                     x > 0;
+                     x--)
+                {
+                    for (var y = BoardBounds.y - 1;
+                         y > 0;
+                         y--)
+                    {
+                        var node = NodeGrid.First(nodePos => nodePos.Position == new Vector2UInt(x, y));
+                        if(node == null) continue;
+                        
+                        Board2048 thisBoard = this;
+                        if (true == node.GetNeighbour(Direction.Top, in thisBoard, out var neighbourNode))
+                        {
+                            MoveNode(ref node, new Vector2UInt(node.Position.x, neighbourNode.Position.y-1));
+                        }
+                        else
+                        {
+                            MoveNode(ref node, new Vector2UInt(node.Position.x, BoardBounds.y - 1));
+                        }
+                    }
+                }
             }
             
             if (direction == Direction.Right)
             {
-                
+                for (var y = BoardBounds.y - 1;
+                     y > 0;
+                     y--)
+                {
+                    for (var x = BoardBounds.x - 1;
+                         x > 0;
+                         x--)
+                    {
+                        var node = NodeGrid.First(nodePos => nodePos.Position == new Vector2UInt(x, y));
+                        if(node == null) continue;
+                    
+                        Board2048 thisBoard = this;
+                        if (true == node.GetNeighbour(Direction.Right, in thisBoard, out var neighbourNode))
+                        {
+                            MoveNode(ref node, new Vector2UInt(neighbourNode.Position.x-1, node.Position.y));
+                        }
+                        else
+                        {
+                            MoveNode(ref node, new Vector2UInt(BoardBounds.x - 1, node.Position.y));
+                        }
+                    }
+                }
             }
             
             if (direction == Direction.Bottom)
@@ -104,13 +146,13 @@ namespace _2048.Core
 
         private void AddNode(Node2048 newNode)
         {
-            NodeGrid.Add(newNode.Position, newNode);
+            NodeGrid.Add(newNode);
             NodeAdded(newNode);
         }
         
         private void RemoveNode(Node2048 node)
         {
-            NodeGrid.Remove(node.Position);
+            NodeGrid.Remove(node);
             NodeRemoved(node);
         }
 
@@ -123,23 +165,45 @@ namespace _2048.Core
                         (uint) Random.Range(0, BoardBounds.y),
                         out Node2048? node))
                 {
-                    NodeGrid.Add(node.Position, node);
+                    if (node != null) 
+                        NodeGrid.Add(node);
                 }
                 else
                 {
                     i--;
                 }
             }
+            
+            CheckPossibleCombinations();
+        }
+
+        private void CheckPossibleCombinations()
+        {
+            Board2048 thisBoard = this;
+            foreach (var node in NodeGrid)
+            {
+                node.CheckPossibleCombinations(in thisBoard);
+            }
         }
         
         public bool CheckForGameOver()
         {
             bool isGameOver = true;
-            foreach (var node in NodeGrid)
+            
+            if (NodeGrid.Count == BoardBounds.x * BoardBounds.y)
             {
-                
+                Board2048 thisBoard = this;
+                foreach (var node in NodeGrid)
+                {
+                    node.CheckPossibleCombinations(in thisBoard);
+                    /*
+                    foreach (var VARIABLE in COLLECTION)
+                    {
+                        if(node.)
+                    }*/
+                }
             }
-
+            
             return isGameOver;
         }
         
--- Assets/Scripts/2048/Core/Node2048.cs
+++ Assets/Scripts/2048/Core/Node2048.cs
@@ -1,77 +1,204 @@
+#nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Miscellaneous.Extensions.Variables;
+using UnityEngine;
 
 namespace _2048.Core
 {
 	public class Node2048
 	{
 		public uint Value { get; private set; }
-		public Vector2UInt Position { get; private set; }
-		
-		private Dictionary<Board2048.Direction, Node2048> _possibleCombinations = new Dictionary<Board2048.Direction, Node2048>(4);
+		public Vector2UInt Position
+		{
+			get => Position;
+			set
+			{
+				Moved(new Vector2(value.x, value.y));
+				Position = value;
+			}
+		}
+		public Action<Vector2> Moved = delegate {  };
+
+		public List<(Board2048.Direction direction, Node2048? possibleNode)> PossibleCombinations { get; private set; }
 
 		public Node2048(Vector2UInt position, uint value)
 		{
 			Position = position;
 			Value = value;
+			PossibleCombinations = new List<(Board2048.Direction direction, Node2048? possibleNode)>(4)
+			{
+				{(Board2048.Direction.Top, null)},
+				{(Board2048.Direction.Right, null)},
+				{(Board2048.Direction.Bottom, null)},
+				{(Board2048.Direction.Left, null)}
+			};
 		}
 
-		public void CheckPossibleCombinations(ref Board2048 board)
+		public void CheckPossibleCombinations(in Board2048 board)
 		{
-			var node = board.NodeGrid.FirstOrDefault(node => node.Value.Equals(this)).Value;
+			var node = board.NodeGrid.First(node => node.Value == this.Value);
+			var topCombination = PossibleCombinations.First(combination => combination.direction == Board2048.Direction.Top);
+			var rightCombination = PossibleCombinations.First(combination => combination.direction == Board2048.Direction.Right);
+			var bottomCombination = PossibleCombinations.First(combination => combination.direction == Board2048.Direction.Bottom);
+			var leftCombination = PossibleCombinations.First(combination => combination.direction == Board2048.Direction.Left);
 			
 			//top
+			if (node.Position.y == board.BoardBounds.y - 1) topCombination.possibleNode = null;
 			for (var y = node.Position.y;
-			     y < board.BoardBounds.x; 
+			     y < board.BoardBounds.y; 
 			     y++)
 			{
-				var possibleNode = board.NodeGrid.FirstOrDefault(nodePos => 
-					nodePos.Key == new Vector2UInt(node.Position.x, y)).Value;
+				var possibleNode = board.NodeGrid.First(nodePos => 
+					nodePos.Position == new Vector2UInt(node.Position.x, y));
 				
 				if (possibleNode == null) continue;
+
+				if (possibleNode.Value == node.Value) 
+					topCombination.possibleNode = possibleNode;
 				
-				if(possibleNode.Value == node.Value) _possibleCombinations.Add(Board2048.Direction.Top, possibleNode);
+				else topCombination.possibleNode = null;
+				
+				break;
 			}
 			
 			//right
+			if (node.Position.x == board.BoardBounds.x - 1) rightCombination.possibleNode = null;
 			for (var x = node.Position.x;
 			     x < board.BoardBounds.x; 
 			     x++)
 			{
-				var possibleNode = board.NodeGrid.FirstOrDefault(nodePos => 
-					nodePos.Key == new Vector2UInt(x, node.Position.y)).Value;
+				var possibleNode = board.NodeGrid.First(nodePos => 
+					nodePos.Position == new Vector2UInt(x, node.Position.y));
 				
 				if (possibleNode == null) continue;
+
+				if (possibleNode.Value == node.Value) 
+					rightCombination.possibleNode = possibleNode;
 				
-				if(possibleNode.Value == node.Value) _possibleCombinations.Add(Board2048.Direction.Right, possibleNode);
+				else rightCombination.possibleNode = null;
+				
+				break;
 			}
 			
 			//bottom
+			if (node.Position.y == 0) bottomCombination.possibleNode = null;
 			for (var y = node.Position.y;
 			     y > 0;
 			     y--)
 			{
-				var possibleNode = board.NodeGrid.FirstOrDefault(nodePos => 
-					nodePos.Key == new Vector2UInt(node.Position.x, y)).Value;
+				var possibleNode = board.NodeGrid.First(nodePos => 
+					nodePos.Position == new Vector2UInt(node.Position.x, y));
 				
 				if (possibleNode == null) continue;
 				
-				if(possibleNode.Value == node.Value) _possibleCombinations.Add(Board2048.Direction.Bottom, possibleNode);
+				if (possibleNode.Value == node.Value) 
+					bottomCombination.possibleNode = possibleNode;
+				
+				else bottomCombination.possibleNode = null;
+				
+				break;
 			}
 			
 			//left
+			if (node.Position.x == 0) leftCombination.possibleNode = null;
 			for (var x = node.Position.x;
 			     x > 0;
 			     x--)
 			{
-				var possibleNode = board.NodeGrid.FirstOrDefault(nodePos => 
-					nodePos.Key == new Vector2UInt(x, node.Position.y)).Value;
+				var possibleNode = board.NodeGrid.First(nodePos => 
+					nodePos.Position == new Vector2UInt(x, node.Position.y));
 				
 				if (possibleNode == null) continue;
 				
-				if (possibleNode.Value == node.Value) _possibleCombinations.Add(Board2048.Direction.Left, possibleNode);
+				if (possibleNode.Value == node.Value) 
+					leftCombination.possibleNode = possibleNode;
+				
+				else leftCombination.possibleNode = null;
+				
+				break;
 			}
+		}
+
+		public bool GetNeighbour(Board2048.Direction direction, in Board2048 board, out Node2048? neighbourNode)
+		{
+			neighbourNode = null;
+			bool isFounded = false;
+
+			if (direction == Board2048.Direction.Top)
+			{
+				for (var y = this.Position.y;
+				     y < board.BoardBounds.y; 
+				     y++)
+				{
+					var possibleNeighbourNode = board.NodeGrid.First(nodePos => 
+						nodePos.Position == new Vector2UInt(this.Position.x, y));
+				
+					if (possibleNeighbourNode == null) continue;
+					else
+					{
+						neighbourNode = possibleNeighbourNode;
+						break;
+					}
+				}
+			}
+			
+			if (direction == Board2048.Direction.Right)
+			{
+				for (var x = this.Position.x;
+				     x < board.BoardBounds.x; 
+				     x++)
+				{
+					var possibleNeighbourNode = board.NodeGrid.First(nodePos => 
+						nodePos.Position == new Vector2UInt(x, this.Position.y));
+				
+					if (possibleNeighbourNode == null) continue;
+					else
+					{
+						neighbourNode = possibleNeighbourNode;
+						break;
+					}
+				}
+			}
+			
+			if (direction == Board2048.Direction.Bottom)
+			{
+				for (var y = this.Position.y;
+				     y > 0;
+				     y--)
+				{
+					var possibleNeighbourNode = board.NodeGrid.First(nodePos => 
+						nodePos.Position == new Vector2UInt(this.Position.x, y));
+				
+					if (possibleNeighbourNode == null) continue;
+					else
+					{
+						neighbourNode = possibleNeighbourNode;
+						break;
+					}
+				}
+			}
+			
+			if (direction == Board2048.Direction.Left)
+			{
+				for (var x = this.Position.x;
+				     x > 0;
+				     x--)
+				{
+					var possibleNeighbourNode = board.NodeGrid.First(nodePos => 
+						nodePos.Position == new Vector2UInt(x, this.Position.y));
+				
+					if (possibleNeighbourNode == null) continue;
+					else
+					{
+						neighbourNode = possibleNeighbourNode;
+						break;
+					}
+				}
+			}
+
+			return isFounded;
 		}
 	}
 }
